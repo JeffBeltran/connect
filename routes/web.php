a83bc62b46b31b7739d5fca731c69a33636154dd
whereisjefe <jeffbeltran@gmail.com>
2017-09-19 13:25:12 -0700

    Basic tests for displaying Threads and single threads

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/threads', 'ThreadController@index')->name('threads.index');
+Route::get('/threads/{thread}', 'ThreadController@show')->name('threads.show');
--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -10,10 +10,26 @@ class ThreadsTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    function a_user_can_browse_threads()
+    function a_user_can_browse_all_threads()
     {
+        $this->withoutExceptionHandling();
+
+        $thread = factory('App\Thread')->create();
+
         $response = $this->get('/threads');
 
-        $response->assertStatus(200);
+        $response->assertSee($thread->title);
+    }
+
+    /** @test */
+    function a_user_can_read_a_single_thread()
+    {
+        $this->withoutExceptionHandling();
+
+        $thread = factory('App\Thread')->create();
+
+        $response = $this->get('/threads/'. $thread->id);
+
+        $response->assertSee($thread->title);
     }
 }
