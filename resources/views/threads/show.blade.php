9e2b7428f2b5b13be25b75a4c86fe1880e82537a
whereisjefe <jeffbeltran@gmail.com>
2017-09-19 17:36:24 -0700

    Users are able to add replies

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -23,5 +23,23 @@
             @endforeach
         </div>
     </div>
+
+    @auth
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <textarea name="body" class="form-control" rows="3" placeholder="Have something to say?"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-large btn-block btn-primary">Post</button>
+                </form>
+            </div>
+        </div>
+    @endauth
+
+    @guest
+        <p class="text-center">Please <a href="{{ route('login') }}">Sign-in</a> to Participate in this discussion</p>
+    @endguest
 </div>
 @endsection
