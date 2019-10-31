f51ea63be145100e815e2526f41eed1f5fa2c550
Jeff Beltran <jeffbeltran@gmail.com>
2019-10-31 09:05:15 -0700

    Upgrade Project to Laravel 6.2

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,18 +5,12 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Seed the application's database.
      *
      * @return void
      */
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        // $this->call(ThreadsTableSeeder::class);
-        // $this->call(RepliesTableSeeder::class);
-        $threads = factory(App\Thread::class, 50)->create();
-        $threads->each(function ($thread) {
-            factory(App\Reply::class, 10)->create(['thread_id' => $thread->id]);
-        });
     }
 }
